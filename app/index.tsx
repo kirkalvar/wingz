283e2b37df7a591eb798401e1e4e2e2f0a0c3d65
Kirk Alvar <kirk.alvar@gmail.com>
2024-09-30 21:33:44 +0800

    Install React Native Map library

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,24 @@
 import React from "react";
+import { StyleSheet } from "react-native";
 import View from "react-native-ui-lib/view";
-import Text from "react-native-ui-lib/text";
+import MapView from "react-native-maps";
 
 const Home = (): React.JSX.Element => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View flex>
+      <MapView style={styles.map} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
 
 export default Home;
