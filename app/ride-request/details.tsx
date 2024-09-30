648f5b008646d7783fcb4b9127c505ce2851d8d0
Kirk Alvar <kirk.alvar@gmail.com>
2024-09-30 20:54:12 +0800

    Initialize routes and remove sample Expo screens

---
+++ app/ride-request/details.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const RideRequestDetails = (): React.JSX.Element => {
+  return (
+    <View>
+      <Text>Ride Request Details</Text>
+    </View>
+  );
+};
+
+export default RideRequestDetails;
