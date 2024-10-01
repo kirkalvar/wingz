df14605823d51984d5424aad4135eab7b084b683
Kirk Alvar <kirk.alvar@gmail.com>
2024-10-02 04:09:29 +0800

    Update Map component

--- components/Map.tsx
+++ components/Map.tsx
@@ -4,7 +4,10 @@ import View from "react-native-ui-lib/view";
 import { Colors } from "react-native-ui-lib";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 
-import type { MarkerProps, RegionProps } from "@/hooks/useRideData";
+import type {
+  MarkerProps,
+  RegionProps,
+} from "@/hooks/useListsRegionAndMarkers";
 import { Circle } from "@/components";
 import { SVGMapMarker } from "@/assets/svgs";
 
@@ -16,8 +19,8 @@ type MapProps = {
 
 const Map = forwardRef(({ region, markers, onPressMarker }: MapProps, ref) => {
   const handleMarkerPress = (e) => {
-    const index = e._targetInst.return.key;
-    onPressMarker(markers[index].id);
+    const id = e._targetInst.return.key;
+    onPressMarker(id);
   };
 
   return (
@@ -30,32 +33,46 @@ const Map = forwardRef(({ region, markers, onPressMarker }: MapProps, ref) => {
         console.log("Map ready");
       }}
     >
-      {markers.map(({ id, coordinate, isRideRequest }: MarkerProps, index) => {
-        return (
-          <Marker
-            key={id}
-            coordinate={coordinate}
-            tracksViewChanges={false}
-            onPress={handleMarkerPress}
-          >
-            <View>
-              {(isRideRequest && (
-                <SVGMapMarker color={Colors.red10} width={40} height={40} />
-              )) || (
-                <Circle
-                  bg-white
-                  size={15}
-                  style={{
-                    borderWidth: 1,
-                    borderColor: Colors.grey50,
-                    backgroundColor: Colors.white,
-                  }}
-                />
-              )}
-            </View>
-          </Marker>
-        );
-      })}
+      {markers.map(
+        ({ id, coordinate, isRideRequest, point }: MarkerProps, index) => {
+          return (
+            <Marker
+              key={id}
+              coordinate={coordinate}
+              tracksViewChanges={false}
+              onPress={handleMarkerPress}
+            >
+              <View>
+                {(isRideRequest && (
+                  <SVGMapMarker
+                    color={point === "pickup" ? Colors.red10 : Colors.grey10}
+                    width={40}
+                    height={40}
+                  />
+                )) || (
+                  <Circle
+                    bg-white
+                    size={30}
+                    style={{
+                      borderWidth: 2,
+                      borderColor: Colors.white,
+                      backgroundColor: Colors.blue30,
+                      shadowColor: "#000",
+                      shadowOffset: {
+                        width: 0,
+                        height: 12,
+                      },
+                      shadowOpacity: 0.58,
+                      shadowRadius: 16.0,
+                      elevation: 24,
+                    }}
+                  />
+                )}
+              </View>
+            </Marker>
+          );
+        }
+      )}
     </MapView>
   );
 });
