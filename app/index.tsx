35d7baf5475e78bca8efcecd980d547ea60924af
Kirk Alvar <kirk.alvar@gmail.com>
2024-10-01 14:09:57 +0800

    Remove ride-requests folder

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,7 @@
-import React, { useRef, useEffect, useCallback } from "react";
+import React, { useRef, useEffect, useCallback, useMemo } from "react";
 import View from "react-native-ui-lib/view";
+import Button from "react-native-ui-lib/button";
+import { useNavigation } from "expo-router";
 
 import { useAppDispatch, useAppSelector } from "@/redux/hook";
 import {
@@ -11,6 +13,7 @@ import { useRideData } from "@/hooks";
 import { Map } from "@/components";
 
 const Home = (): React.JSX.Element => {
+  const navigation = useNavigation();
   const mapRef = useRef(null);
   const dispatch = useAppDispatch();
   const { user, rideRequests } = useAppSelector(selectRide);
@@ -21,6 +24,12 @@ const Home = (): React.JSX.Element => {
 
   // console.log("markers", markers);
   // console.log("region", region);
+
+  const handleOnPress = useMemo(() => {
+    navigation.navigate("ride-request", {
+      id: "8fa57ef6-89b5-47a1-b07e-83bd7686aa3f",
+    });
+  }, [navigation]);
 
   const init = useCallback(async () => {
     const intialCoords = await dispatch(getUseCurrentLocation()).unwrap();
@@ -35,12 +44,13 @@ const Home = (): React.JSX.Element => {
 
   return (
     <View flex>
-      <Map
+      <Button label="View" onPress={handleOnPress} />
+      {/*<Map
         ref={mapRef}
         region={region}
         markers={markers}
         onPressMarker={() => {}}
-      />
+      />*/}
     </View>
   );
 };
