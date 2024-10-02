521044d57767218cc0424d08f6b70731f94e3b75
Kirk Alvar <kirk.alvar@gmail.com>
2024-10-02 08:37:33 +0800

    Update comment

--- redux/actions/rideAction.tsx
+++ redux/actions/rideAction.tsx
@@ -86,7 +86,7 @@ export const fetchRideRequests = createAsyncThunk<
     const randomCoordinates = generateRandomCoordinates(
       latitude,
       longitude,
-      1000, // 100 meters radius
+      1000, // 1000 meters radius
       22
     );
 
